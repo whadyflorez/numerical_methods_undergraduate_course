935085d1a15ff492520a9076e2bf760436acf77b
Whady Felipe Florez <whady.florez@upb.edu.co>
2025-03-26 08:23:26 -0500

    Update implicito_FD_Robin.py

--- implicito_FD_Robin.py
+++ implicito_FD_Robin.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 
 # Parámetros
 c = 1
-h = 5.0
+h = 1.0
 x_min, x_max = -1, 1
-dx = 0.01
+dx = 0.1
 dt = 0.01
 r = c * dt / dx**2
-t_max = 0.1
+t_max = 1
 
 # Mallas
 x = np.arange(x_min, x_max + dx, dx)
@@ -16,7 +16,8 @@ nx = len(x)
 nt = int(t_max / dt)
 
 # Condición inicial
-u = np.ones(nx) * 2
+u = np.ones(nx)
+
 u_history = [u.copy()]
 time_steps = [0]
 
@@ -25,40 +26,58 @@ A = np.zeros((nx, nx))
 
 # Interior
 for i in range(1, nx - 1):
-    A[i, i - 1] = -r
-    A[i, i]     = 1 + 2*r
-    A[i, i + 1] = -r
+    A[i, i - 1] = r
+    A[i, i]     = -1 - 2*r
+    A[i, i + 1] = r
 
 # Frontera izquierda (Robin)
-A[0, 0] = 1 + dx * h
-A[0, 1] = -1
+
+A[0, 0] = -3 - 2*dx * h
+A[0, 1] = 4
+A[0, 2] = -1 
 
 # Frontera derecha (Robin)
-A[-1, -2] = 1
-A[-1, -1] = -(1 + dx * h)
+
+A[-1, -1] = 3+2*dx*h
+A[-1, -2] = -4
+A[-1, -3] = 1
+
+b = -u.copy()
+    # Modificar términos independientes con valor 10 de Robin
+b[0] = -2*dx * h * 10
+b[-1]= 2*dx * h * 10
 
 # Avance temporal
 for n in range(1, nt + 1):
-    b = u.copy()
-
-    # Modificar términos independientes con valor 10 de Robin
-    b[0] += dx * h * 10
-    b[-1] += -dx * h * 10
 
     # Resolver sistema lineal
     u = np.linalg.solve(A, b)
-
     u_history.append(u.copy())
     time_steps.append(n * dt)
+    b[1:nx-1]=-u[1:nx-1]
+    b[0] = -2*dx * h * 10
+    b[-1]= 2*dx * h * 10
 
 # --- GRAFICA u vs x ---
 plt.figure(figsize=(10,6))
 for i, u_plot in enumerate(u_history):
-    if i%5==0:
+    if i%1==0:
         plt.plot(x, u_plot, label=f't = {time_steps[i]:.3f}')
 plt.title("u vs x (Implícito con Robin)")
 plt.xlabel("x")
 plt.ylabel("u(x,t)")
 plt.legend()
+plt.grid(True)
+plt.show()
+
+# --- GRAFICA u vs t en x = 0 ---
+x0_index = nx // 2
+u_vs_t = [u_hist[x0_index] for u_hist in u_history]
+
+plt.figure(figsize=(8,5))
+plt.plot(time_steps, u_vs_t, marker='o')
+plt.title("Evolución temporal de u en x = 0 (implícito)")
+plt.xlabel("Tiempo t")
+plt.ylabel("u(x=0,t)")
 plt.grid(True)
 plt.show()
