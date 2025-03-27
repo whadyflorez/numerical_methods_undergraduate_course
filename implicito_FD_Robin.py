f9b31f710467fc13bd4905d5e3ed065eafe1d8c2
Whady Felipe Florez <whady.florez@upb.edu.co>
2025-03-27 08:56:31 -0500

    Update implicito_FD_Robin.py

--- implicito_FD_Robin.py
+++ implicito_FD_Robin.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 # Parámetros
 c = 1
-h = 1.0
+h = 5.0
 x_min, x_max = -1, 1
-dx = 0.1
+dx = 0.01
 dt = 0.01
 r = c * dt / dx**2
 t_max = 1
@@ -31,21 +31,28 @@ for i in range(1, nx - 1):
     A[i, i + 1] = r
 
 # Frontera izquierda (Robin)
+A[0,0]=-1-h*dx
+A[0,1]=1
 
-A[0, 0] = -3 - 2*dx * h
-A[0, 1] = 4
-A[0, 2] = -1 
+# A[0, 0] = -3 - 2*dx * h
+# A[0, 1] = 4
+# A[0, 2] = -1 
 
 # Frontera derecha (Robin)
+A[-1,-1]=1+h*dx
+A[-1,-2]=-1
+# A[-1, -1] = 3+2*dx*h
+# A[-1, -2] = -4
+# A[-1, -3] = 1
 
-A[-1, -1] = 3+2*dx*h
-A[-1, -2] = -4
-A[-1, -3] = 1
-
-b = -u.copy()
+b=np.zeros(nx)
+b[1:nx-1] = -u[1:nx-1]
     # Modificar términos independientes con valor 10 de Robin
-b[0] = -2*dx * h * 10
-b[-1]= 2*dx * h * 10
+b[0] = -dx * h * 10
+b[nx-1]= dx * h * 10
+
+# b[0] = -2*dx * h * 10
+# b[-1]= 2*dx * h * 10
 
 # Avance temporal
 for n in range(1, nt + 1):
@@ -55,13 +62,16 @@ for n in range(1, nt + 1):
     u_history.append(u.copy())
     time_steps.append(n * dt)
     b[1:nx-1]=-u[1:nx-1]
-    b[0] = -2*dx * h * 10
-    b[-1]= 2*dx * h * 10
+    b[0] = -dx * h * 10
+    b[-1]= dx * h * 10
+
+    # b[0] = -2*dx * h * 10
+    # b[-1]= 2*dx * h * 10
 
 # --- GRAFICA u vs x ---
 plt.figure(figsize=(10,6))
 for i, u_plot in enumerate(u_history):
-    if i%1==0:
+    if i%5==0:
         plt.plot(x, u_plot, label=f't = {time_steps[i]:.3f}')
 plt.title("u vs x (Implícito con Robin)")
 plt.xlabel("x")
