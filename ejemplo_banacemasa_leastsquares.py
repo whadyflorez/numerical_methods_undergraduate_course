f0ba5711f4b3c0d83f4bb16623a9c4fee9f9f445
Whady Felipe Florez <whady.florez@upb.edu.co>
2025-07-25 10:57:39 -0500

    u

--- ejemplo_banacemasa_leastsquares.py
+++ ejemplo_banacemasa_leastsquares.py
@@ -6,20 +6,29 @@ Created on Thu Jul 24 06:10:31 2025
 @author: whadymacbook2016
 """
 import numpy as np
-from scipy.linalg import solve,det,lstsq
+from scipy.linalg import solve,det,lstsq,lu
 
-#x5=30
+
 A=np.array([[1,1,0,0,0],[1,0,-1,0,-1],[0,1,1,-1,0],[0,0,0,1,1]])
 rhs=np.array([100,0,0,100])
 
-#d=det(A)
+
 x=lstsq(A,rhs)
-
 solucion=x[0]
+print(np.matmul(A,solucion))
+print(np.linalg.matrix_rank(A))
+P,L,U=lu(A)
 
 
-AA=np.array([[1,1,0,0],[0,1,1,-1],[1,0,-1,1],[1,0,0,0]])
-print(np.linalg.det(AA))
+#redefinir el problema x1=60,x3=10
+A=np.array([[1,1,0,0,0],[0,1,1,-1,0],[0,0,0,1,1],[1,0,0,0,0],[0,0,1,0,0]])
+rhs=np.array([100,0,100,60,10])
+xx=solve(A,rhs)
+print(det(A))
+
+
+
+
 
 
 
