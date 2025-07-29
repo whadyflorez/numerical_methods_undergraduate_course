2948d7acbcceb199db904cf492db9e9bab08758b
Whady Felipe Florez <whady.florez@upb.edu.co>
2025-07-29 07:22:16 -0500

    u

--- ejemplo_aprox_cuadratica.py
+++ ejemplo_aprox_cuadratica.py
@@ -13,23 +13,24 @@ x=np.array([0,1,2,2.5,4,7,3.2,6])
 y=np.array([0.5,0.7,3.5,7,4.5,10,20,9])
 
 
-A=np.zeros((8,2))
-A[:,0]=x
-A[:,1]=1.0
+A=np.zeros((8,3))
+A[:,0]=x**2
+A[:,1]=x
+A[:,2]=1.0
 
 p=lstsq(A,y)
 param=p[0]
 
-nplot=50
+nplot=100
 xplot=np.linspace(0,7,nplot)
 yplot=[]
 for i in range(nplot):
-    yaprox=param[0]*xplot[i]+param[1]
+    yaprox=param[0]*xplot[i]**2+param[1]*xplot[i]+param[2]
     yplot.append(yaprox)
     
 plt.figure()
-plt.plot(xplot,yplot,'--')
-plt.plot(x,y,'o') 
+plt.plot(xplot,yplot,'g:')
+plt.plot(x,y,'+') 
 plt.xlabel('valores x')
 plt.ylabel('valores y')   
     
