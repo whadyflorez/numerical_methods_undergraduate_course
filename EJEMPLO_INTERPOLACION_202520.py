2da0f54e975ed9fd6c7cf3ae903d6737f8f8b458
Whady Felipe Florez <whady.florez@upb.edu.co>
2025-07-31 06:54:14 -0500

    u

--- EJEMPLO_INTERPOLACION_202520.py
+++ EJEMPLO_INTERPOLACION_202520.py
@@ -13,24 +13,30 @@ x=np.array([0,1,2,2.5,4,7,3.2,6])
 y=np.array([0.5,0.7,3.5,7,4.5,10,20,9])
 
 
-A=np.zeros((8,2))
-A[:,0]=x
-A[:,1]=1.0
+A=np.zeros((8,8))
+A[:,0]=1
+A[:,1]=x
+A[:,2]=x**2 
+A[:,3]=x**3
+A[:,4]=x**4
+A[:,5]=x**5
+A[:,6]=x**6
+A[:,7]=x**7
 
-p=lstsq(A,y)
-param=p[0]
 
-nplot=50
+p=solve(A,y)
+
+
+nplot=100
 xplot=np.linspace(0,7,nplot)
 yplot=[]
 for i in range(nplot):
-    yaprox=param[0]*xplot[i]+param[1]
+    yaprox=p[0]+p[1]*xplot[i]+p[2]*xplot[i]**2+p[3]*xplot[i]**3+\
+    p[4]*xplot[i]**4+p[5]*xplot[i]**5+p[6]*xplot[i]**6+p[7]*xplot[i]**7    
     yplot.append(yaprox)
     
 plt.figure()
-plt.plot(xplot,yplot,'r:')
-plt.plot(x,y,'go') 
+plt.plot(xplot,yplot,'g:')
+plt.plot(x,y,'+') 
 plt.xlabel('valores x')
 plt.ylabel('valores y')   
-    
-    
---
+++ ejemplo_polyfit_202520.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 31 06:03:46 2025
+
+@author: whadymacbook2016
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.array([0.0, 1.0, 2.0, 3.0,  4.0,  5.0])
+y = np.array([0.0, 0.8, 0.9, 0.1, -0.8, -1.0])
+z = np.polyfit(x, y, 2)
+
+p = np.poly1d(z)
+print(p(1.0))
+
+xp = np.linspace(-2, 6, 100)
+yp=p(xp)
+plt.plot(x, y, 'o', xp, yp, '--')
