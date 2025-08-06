8e5b8a1009307cf98950b88ab3c4efe72aefb001
Whady Felipe Florez <whady.florez@upb.edu.co>
2025-08-06 12:58:16 -0500

    u

--- ejemplo_polyfit_202520.py
+++ ejemplo_polyfit_202520.py
@@ -8,8 +8,8 @@ Created on Thu Jul 31 06:03:46 2025
 import numpy as np
 import matplotlib.pyplot as plt
 x = np.array([0.0, 1.0, 2.0, 3.0,  4.0,  5.0])
-y = np.array([0.0, 0.8, 0.9, 0.1, -0.8, -1.0])
-z = np.polyfit(x, y, 5)
+y = np.array([0.0, 1.0, 4, 9, 16, 25])
+z = np.polyfit(x, y, 2)
 
 p = np.poly1d(z)
 print(p(1.0))
