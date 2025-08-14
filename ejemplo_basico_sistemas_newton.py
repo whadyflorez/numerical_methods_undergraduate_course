e0779ceffe9b44295925fe002e2bd7d61e4e7674
Whady Felipe Florez <whady.florez@upb.edu.co>
2025-08-14 07:19:58 -0500

    u

--- ejemplo_basico_sistemas_newton.py
+++ ejemplo_basico_sistemas_newton.py
@@ -7,24 +7,28 @@ Created on Wed Aug 13 12:27:17 2025
 """
 import numpy as np
 from numpy.linalg import solve,norm
+from scipy.optimize import fsolve
+
 
 def F(X):
     return np.array([X[0]+X[1]-1,X[0]**2+X[1]**2-1])
 
-def J(X):
-    jac=np.array([[1,1],[2*X[0],2*X[1]]])
-    return jac
+# def J(X):
+#     jac=np.array([[1,1],[2*X[0],2*X[1]]])
+#     return jac
 
 X0=np.array([1,1.5])
+solucion=fsolve(F,X0)
+print(f"La primera variable es {solucion[0]:.3e} ")
 
 
-for _ in range(30):
-    Error=F(X0)
-    sigma=norm(Error)
-    print(sigma)
-    M=J(X0)
-    B=-F(X0)
-    dX=solve(M,B)
-    X1=X0+0.25*dX
-    X0=X1
+# for _ in range(30):
+#     Error=F(X0)
+#     sigma=norm(Error)
+#     print(sigma)
+#     M=J(X0)
+#     B=-F(X0)
+#     dX=solve(M,B)
+#     X1=X0+0.25*dX
+#     X0=X1
 
