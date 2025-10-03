1d74bfd187f573dce01dd0aa4bbf0ef5a613ec42
Whady Felipe Florez <whady.florez@upb.edu.co>
2025-10-03 13:22:37 -0500

    u

--- ejemplo_aletas_fd.py
+++ ejemplo_aletas_fd.py
@@ -1,59 +1,69 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct  1 12:42:00 2025
+Created on Fri Oct  3 12:47:48 2025
 
 @author: whadymacbook2016
 """
 import numpy as np
-from numpy.linalg import solve
 import matplotlib.pyplot as plt
 
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 (necesario para la proyección 3D)
 
 
-n=25
-L=10 
-H=10
-dx=L/4
+n=35
+L=2
+H=1
+dx=L/6
 dy=H/4
 
-A=np.zeros((n,n))
-B=np.zeros(n)
+A=np.zeros((35,35))
+B=np.zeros(35)
 
-L1=[6,7,8,11,12,13,16,17,18]
-L2=[20,21,22,23,24]
-L3=[15,10,5,0,1,2,3,4,9,14,19]
+L1=[8,9,10,11,12,15,16,17,18,19,22,23,24,25,26]
+L2=[0,7,14,21,28]
+L3=[29,30,31,32,33,34]
+L4=[1,2,3,4,5,6]
+L5=[27,20,13]
+
 
 for i in L1:
-    A[i,i-1]=1/dx**2
     A[i,i+1]=1/dx**2
-    A[i,i-5]=1/dy**2
-    A[i,i+5]=1/dy**2
-    A[i,i]=-2/dx**2-2/dy**2 
+    A[i,i-1]=1/dx**2
+    A[i,i+7]=1/dy**2
+    A[i,i-7]=1/dy**2
+    A[i,i]=-2/dx**2-2/dy**2-0.1
     B[i]=0
 for i in L2:
     A[i,i]=1
-    B[i]=1
+    B[i]=10
 for i in L3:
-    A[i,i]=1
-    B[i]=0   
-    
-    
-plt.spy(A)   
+    A[i,i-7]=-1/dx
+    A[i,i]=1/dx+1
+    B[i]=20 
+for i in L4:
+    A[i,i+7]=1/dy
+    A[i,i]=-1/dy-1
+    B[i]=-20 
+for i in L5:
+    A[i,i]=1/dx
+    A[i,i-1]=-1/dx
+    B[i]=0
 
-u=solve(A,B) 
-    
-V=np.zeros((5,5)) 
-V[0,:]=u[20:25]
-V[1,:]=u[15:20]    
-V[2,:]=u[10:15] 
-V[3,:]=u[5:10]
-V[4,:]=u[0:5]   
+plt.spy(A)
+
+T=np.linalg.solve(A,B)
+
+V=np.zeros((5,7))
+V[0,:]=T[28:35]
+V[1,:]=T[21:28]
+V[2,:]=T[14:21]
+V[3,:]=T[7:14]
+V[4,:]=T[0:7]
 
 
 # Malla de coordenadas (5x5 porque V es 5x5)
-x = np.linspace(0, L, 5)
+x = np.linspace(0, L, 7)
 y = np.linspace(0, H, 5)
 X, Y = np.meshgrid(x, y)
 
@@ -73,11 +83,66 @@ plt.tight_layout()
 plt.show()    
     
     
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     
     
     
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
